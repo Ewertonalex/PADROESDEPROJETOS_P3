656c719f538faa167f37de437c545929e0764fff
Ewerton Alexander <91163104+Ewertonalex@users.noreply.github.com>
2022-09-08 21:43:31 -0300

    tedheranca

--- auladerevisao/src/TedHeranca/Principal.java
+++ auladerevisao/src/TedHeranca/Principal.java
@@ -5,12 +5,9 @@ public class Principal {
         Funcionario funcionario = new Funcionario("José", "12.05.1980", "Av Epitacio", "839888-5566","Professor"
                 , 8562, 2.500, "10.04.2020");
 
-
-
         Cliente cliente = new Cliente("Alberto", "19.05.1995", "Rua do Sol", "83987556696", "Gerente", "8456",
                 "Administrador");
 
         System.out.println("Dados do funcionário: ");
     }
-
 }
